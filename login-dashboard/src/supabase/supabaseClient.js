ad8f369e0d3991105ba7a027bc6bac84ec12c28c
HSp4m <sp4m787@gmail.com>
2024-05-12 02:02:27 -0400

    💥 fix: Revert inefficient changes

--- login-dashboard/src/supabase/supabaseClient.js
+++ login-dashboard/src/supabase/supabaseClient.js
@@ -2,7 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.SUPABASE_APP_URL;
 const supabaseKey = import.meta.env.SUPABASE_APP_ANON_KEY;
-console.log(supabaseUrl)
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
