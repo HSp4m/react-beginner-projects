304b381fa736ac5e882358060b3806ff25410dcb
HSp4m <sp4m787@gmail.com>
2024-05-12 01:59:06 -0400

    💥 fix: Revert inefficient changes

--- login-dashboard/src/env.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-const envSchema = z.object({
-  SUPABASE_APP_URL: z.string().url(),
-  SUPABASE_APP_ANON_KEY: z.string()
-});
-
-export const env = envSchema.parse(process.env)
--- login-dashboard/src/supabase/supabaseClient.js
+++ login-dashboard/src/supabase/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-import { env } from '../env'
 
-const supabaseUrl = env.SUPABASE_APP_URL;
-const supabaseKey = env.SUPABASE_APP_ANON_KEY;
+const supabaseUrl = import.meta.env.SUPABASE_APP_URL;
+const supabaseKey = import.meta.env.SUPABASE_APP_ANON_KEY;
+console.log(supabaseUrl)
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
