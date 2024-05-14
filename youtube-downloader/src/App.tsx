69dff127d9de372dfeb3889417358bbf0ec1bc26
HSp4m <sp4m787@gmail.com>
2024-05-14 16:20:05 -0400

    💄 feat: Add DarkMode

--- youtube-downloader/src/App.tsx
+++ youtube-downloader/src/App.tsx
@@ -1,8 +1,15 @@
+import { ModeToggle } from "./components/mode-toggle";
+
 export function App() {
  
-
   return (
-  <h1>Hello world</h1>
-  )
+    <div className="p-6 max-w-4xl mx-auto space-y-4">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold">Youtube downloader</h1>
+        <ModeToggle />
+      </div>
+    </div>
+  );
+
 }
 
